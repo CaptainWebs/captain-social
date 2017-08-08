d873138479f3a71a45853b49ef68084171307a12
Nurlan Isazade <isazaden@yahoo.com>
2017-08-08 10:52:42 +0100

    Firebase-get completed

--- captainwebs-social/FeedVCV.swift
+++ captainwebs-social/FeedVCV.swift
@@ -13,6 +13,8 @@ import SwiftKeychainWrapper
 class FeedVCV: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView : UITableView!
+  
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +24,23 @@ class FeedVCV: UIViewController, UITableViewDelegate, UITableViewDataSource {
       
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
           
-           print(snapshot.value)
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot]{
+            
+              for snap in snapshot{
+              
+                print("Snap: \(snap)")
+                if let postDict = snap.value as? Dictionary<String, AnyObject>{
+                
+                  let key = snap.key
+                  let post = Post(postKey: key, postData: postDict)
+                  self.posts.append(post)
+                
+                }
+              
+              }
+      
+            }
+            self.tableView.reloadData()
           
         })
       
@@ -36,12 +54,15 @@ class FeedVCV: UIViewController, UITableViewDelegate, UITableViewDataSource {
   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
-      return 3
+      return posts.count
 
 
     }
   
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+      let post = posts[indexPath.row]
+      print("Nurlan: \(post.caption)")
 
       return tableView.dequeueReusableCell(withIdentifier: "feedCell") as! PostCell
    
